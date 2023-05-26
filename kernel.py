724ab6adb604804c622961c321a6e314f8f72da2
user1095108 <janezz55@gmail.com>
2023-05-26 19:59:15 +0200

    some fixes

--- kernel.py
+++ kernel.py
@@ -4,30 +4,22 @@ from IPython.display import HTML, Image
 from metakernel import MetaKernel, Magic
 
 class GPPMagics(Magic):
-  def line_CC(self, args=''):
-    self.kernel._vars["CC"] = args
+  def line_CC(self, a=''):
+    self.kernel._vars["CC"] = a if a else (self.kernel.Print(self.kernel._vars["CC"]) or self.kernel._vars["CC"])
 
-  def line_CFLAGS(self, args=''):
-    self.kernel._vars["CFLAGS"] = args
+  def line_CFLAGS(self, a=''):
+    self.kernel._vars["CFLAGS"] = a if a else (self.kernel.Print(self.kernel._vars["CFLAGS"]) or self.kernel._vars["CFLAGS"])
 
-  def line_cd(self, args=''):
-    if args:
-      os.chdir(os.path.expanduser(args))
-    else:
-      self.kernel.Print(os.getcwd())
+  def line_cd(self, a=''):
+    os.chdir(os.path.expanduser(a)) if a else self.kernel.Print(os.getcwd())
 
-  def line_get(self, args=''):
-    self.kernel.Print(self.kernel._vars.get(args, f"{args} does not exist") if args else str(self.kernel._vars))
-
-  def line_reset(self, args=''):
+  def line_reset(self, a=''):
     self.kernel._vars = {
       "CC": "g++",
       "CFLAGS": "-std=c++20 -O3 -s -fno-stack-protector"
     }
 
 class GPPKernel(MetaKernel):
-  _dir = tempfile.gettempdir()
-
   implementation = 'Jupyter GPP Kernel'
   implementation_version = '0.1'
   language = 'c++'
@@ -38,27 +30,19 @@ class GPPKernel(MetaKernel):
   }
   banner = implementation
 
-  def _exec_gpp(self, code):
-    with tempfile.NamedTemporaryFile(dir=self._dir, suffix=".out") as tmpfile:
-      filename = tmpfile.name
-
+  def _exec_gpp(self, filename, code):
     return subprocess.run(
         f"{self._vars['CC']} {self._vars['CFLAGS']} -xc++ -o{filename} -&&{filename}",
         input=code.encode(),
         capture_output=True,
-        cwd=self._dir,
         shell=True,
       )
 
-  def _exec_puml(self, code):
-    with tempfile.NamedTemporaryFile(dir=self._dir, suffix=".out") as tmpfile:
-      filename = tmpfile.name
-
+  def _exec_puml(self, filename, code):
     return subprocess.run(
         "plantuml -tpng -p",
         input=code.encode(),
         capture_output=True,
-        cwd=self._dir,
         shell=True,
       )
 
@@ -68,11 +52,11 @@ class GPPKernel(MetaKernel):
     self.call_magic("%reset")
 
   def do_execute_direct(self, code, silent=False):
-    if code.lstrip().startswith("@start"):
-      result = self._exec_puml(code)
-    else:
-      result = self._exec_gpp(code)
+    with tempfile.NamedTemporaryFile(dir=tempfile.gettempdir(), suffix=".out") as tmpfile:
+      tmpfilename = tmpfile.name
 
+    result = self._exec_puml(tmpfilename, code) if code.lstrip().startswith("@start") else self._exec_gpp(tmpfilename, code)
+    os.remove(tmpfilename) if os.path.isfile(tmpfilename) else None
     output = result.stderr if result.returncode else result.stdout
 
     self.kernel_resp = {
