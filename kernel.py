38026e0dffb1f6a87131280115cce365dcfadb88
user1095108 <janezz55@gmail.com>
2023-05-28 06:15:47 +0200

    some fixes

--- kernel.py
+++ kernel.py
@@ -10,6 +10,9 @@ class GPPMagics(Magic):
   def line_CFLAGS(self, a=''):
     self.kernel._vars["CFLAGS"] = a
 
+  def line_CPPFLAGS(self, a=''):
+    self.kernel._vars["CPPFLAGS"] = a
+
   def line_LDFLAGS(self, a=''):
     self.kernel._vars["LDFLAGS"] = a
 
@@ -25,6 +28,9 @@ class GPPMagics(Magic):
   def line_OCTAVE(self, a=''):
     self.kernel._cellcontents = "octave"
 
+  def line_PUML(self, a=''):
+    self.kernel._cellcontents = "puml"
+
   def line_cd(self, a=''):
     os.chdir(os.path.expanduser(a)) if a and os.path.isdir(os.path.expanduser(a)) else self.kernel.Print(os.getcwd())
 
@@ -34,7 +40,8 @@ class GPPMagics(Magic):
   def line_reset(self, a=''):
     self.kernel._vars = {
         "CC": "g++",
-        "CFLAGS": "-std=c++20 -march=native -O3 -fno-plt -fno-stack-protector -s -pipe",
+        "CFLAGS": "-std=c18 -march=native -O3 -fno-plt -fno-stack-protector -s -pipe",
+        "CPPFLAGS": "-std=c++20 -march=native -O3 -fno-plt -fno-stack-protector -s -pipe",
         "LDFLAGS": "",
         "OFLAGS": "",
         "PFLAGS": "-tpng -darkmode",
@@ -58,9 +65,10 @@ class GPPKernel(MetaKernel):
       filename = tmpfile.name
 
     lang = "c" if self._vars['CC'] in ('gcc', 'clang') or "c" == self._cellcontents else "c++"
+    flags = self._vars['CFLAGS' if 'c' == lang else 'CPPFLAGS']
 
     result = subprocess.run(
-        f"{self._vars['CC']} {self._vars['CFLAGS']} -x{lang} {self._vars['LDFLAGS']} -o{filename} -&&{filename}",
+        f"{self._vars['CC']} {flags} -x{lang} {self._vars['LDFLAGS']} -o{filename} -&&{filename}",
         input=code.encode(),
         capture_output=True,
         shell=True,
@@ -89,24 +97,27 @@ class GPPKernel(MetaKernel):
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
     self.register_magics(GPPMagics)
-    self.call_magic("%reset")
+    self.call_magic('%reset')
 
   def do_execute_direct(self, code, silent=False):
-    result = self._exec_octave(code) if 'octave' == self._cellcontents else self._exec_puml(code) if code.lstrip().startswith("@start") else self._exec_gpp(code)
-    output = result.stderr if result.returncode else result.stdout
-
+    result = self._exec_octave(code) if 'octave' == self._cellcontents else self._exec_puml(code) if 'puml' == self._cellcontents else self._exec_gpp(code)
     self._cellcontents = ""
+
     self.kernel_resp = {
         'execution_count': self.execution_count,
         'payload': [],
-        'status': "error" if result.returncode else 'ok',
+        'status': 'error' if result.returncode else 'ok',
       }
 
     if result.returncode:
+      output = result.stderr if result.stderr else result.stdout
       self.Error(output.decode())
-    elif output.startswith(b'\x89PNG\r\n\x1a\n') or output.startswith(b'\xFF\xD8'):
-      return Image(output)
     else:
-      output = output.decode()
+      output = result.stdout
 
-      return HTML(output) if re.search(r'<svg[^>]*>', output) else self.Write(output)
+      if output.startswith(b'\x89PNG\r\n\x1a\n') or output.startswith(b'\xFF\xD8'):
+        return Image(output)
+      else:
+        output = output.decode()
+
+        return HTML(output) if re.search(r'<svg[^>]*>', output) else self.Write(output)
